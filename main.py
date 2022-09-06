b5215ea995930a2bd54eff61e11d151a6e75acfb
Tautvydas-coder <b7&*%13kHqvP>
2022-09-06 16:50:59 +0300

    refactored

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from resources.page_info import *
+from python_resources.fetch_data import *
+from python_resources.write_data import *
+from python_resources.mark_web_element import *
 
 if __name__ == '__main__':
     page = check_web_status()
-    # web_root = fetch_page_content(page)
-    # web_tree = fetch_root_tree(web_root)
-    # web_results = fetch_web_element_info(web_root)
+    web_root = fetch_page_content(page)
+    web_tree = fetch_root_tree(web_root)
+    web_results = fetch_web_element_info(web_root)
     # vocabulary = fetch_data_from_txt()
-    # write_to_csv(web_results, web_tree, web_root, vocabulary)
+    list = write_to_csv(web_results, web_tree)
+    print(list)
+    highlight(list)
     # json_list = fetch_json_list()
     # json_string = fetch_json_format(json_list)
     # write_to_json_file(json_string)
