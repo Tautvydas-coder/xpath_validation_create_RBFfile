eed74fa7d4ae3059b86989ae26b7d3d532620984
Tautvydas-coder <b7&*%13kHqvP>
2022-09-07 12:35:26 +0300

    added create robot file function

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from python_resources.fetch_data import *
+from python_resources.general_functions import *
 from python_resources.write_data import *
 from resources.page_info import check_web_status
 
 if __name__ == '__main__':
-    page = check_web_status()
-    web_root = fetch_page_content(page)
-    web_tree = fetch_root_tree(web_root)
-    web_results = fetch_web_element_info(web_root)
-    write_to_csv(web_results, web_tree)
+    # page = check_web_status()
+    # web_root = fetch_page_content(page)
+    # web_tree = fetch_root_tree(web_root)
+    # web_results = fetch_web_element_info(web_root)
+    # write_to_csv(web_results, web_tree)
+    create_robot_file()
     # print(list)
     # check_element_visibility(list)
     # json_list = fetch_json_list()
