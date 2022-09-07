7a36633d70ec7f3835c8332a6e27940fd240e287
Tautvydas-coder <b7&*%13kHqvP>
2022-09-07 12:34:38 +0300

    refactor

--- python_resources/write_data.py
+++ python_resources/write_data.py
@@ -1,4 +1,5 @@
-from python_resources.check_element_visibility import *
+from python_resources.check_elem_visibility import *
+
 
 def write_to_json_file(json_string):
     with open(json_name, 'w') as json_file:
@@ -9,5 +10,5 @@ def write_to_csv(results, tree):
     with open(csv_name, 'w') as file:
         for result in results:
             xpath = tree.getpath(result)
-            answer=check_element_visibility(xpath)
+            answer = check_element_visibility(xpath)
             file.write(f"{answer}\n")
