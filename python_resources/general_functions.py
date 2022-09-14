924bcdf8c5ba138b6de620629b6190907cdf17c4
Tautvydas-coder <tautvydas.kvietkauskas@orioninc.com>
2022-09-14 10:03:38 +0300

    refactor

--- python_resources/general_functions.py
+++ python_resources/general_functions.py
@@ -1,7 +1,6 @@
 from main import *
 
 
-def create_robot_file(settings_variable_temple,test_cases_temple,keyword_temple):
+def create_robot_file(settings_variable_temple, test_cases_temple, keyword_temple):
     with open(robot_name, 'w') as robot_file:
-        robot_file.write(settings_variable_temple+test_cases_temple+keyword_temple)
-
+        robot_file.write(settings_variable_temple + test_cases_temple + keyword_temple)
