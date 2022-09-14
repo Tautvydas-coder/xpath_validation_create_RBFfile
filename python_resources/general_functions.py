cf0520cb33bf2fa2fc0fe789d778bc4fa3931cc6
SETaKv <EXTTautvydas.Kvietkauskas@ascom.com>
2022-09-14 15:24:09 +0300

    added URl

--- python_resources/general_functions.py
+++ python_resources/general_functions.py
@@ -1,8 +1,9 @@
 from main import *
-
+from resources.variables import URL
 
 def create_robot_file(settings_variable_temple, visible_xpaths_list, test_cases_temple, keyword_temple):
     with open(robot_name, 'w') as robot_file:
+        robot_file.write(f"#URL - {URL}\n")
         robot_file.write(settings_variable_temple + test_cases_temple)
         for xpath in visible_xpaths_list:
             robot_file.write(f"   Element Should Be Visible  {xpath}\n")
