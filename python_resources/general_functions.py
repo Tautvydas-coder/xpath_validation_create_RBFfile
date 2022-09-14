44ebb6848b88c113f1cf5260f55e399239bcbc1d
Tautvydas-coder <tautvydas.kvietkauskas@orioninc.com>
2022-09-14 10:59:35 +0300

    write xpaths to robot file

--- python_resources/general_functions.py
+++ python_resources/general_functions.py
@@ -1,6 +1,9 @@
 from main import *
 
 
-def create_robot_file(settings_variable_temple, test_cases_temple, keyword_temple):
+def create_robot_file(settings_variable_temple, visible_xpaths_list, test_cases_temple, keyword_temple):
     with open(robot_name, 'w') as robot_file:
-        robot_file.write(settings_variable_temple + test_cases_temple + keyword_temple)
+        robot_file.write(settings_variable_temple + test_cases_temple)
+        for xpath in visible_xpaths_list:
+            robot_file.write(f"  Element Should Be Visible  {xpath}\n")
+        robot_file.write(keyword_temple)
